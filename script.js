f4a27235c24a15263840523ddfa30ddd6bf2d8f1
Merrin <merrin.macleod@gmail.com>
2015-06-13 16:48:21 +1000

    sun resets on click

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
 
   $('body').on( "mousemove", colourChange);
+  $('body').on( "click", colourReset);
 
 });
 
@@ -18,9 +19,25 @@ function colourChange() {
   }
   bluePercent = bluePercent + 0.2;
   // insert new colours
+  colourInsert();
+}
+
+function colourReset() {
+  colorsRGB = [ [113, 128, 168], [115, 128, 160], [139, 151, 165], [145, 152, 130], [145, 140, 108], [137, 114, 65], [102, 82, 57] ]
+  bluePercent = 70;
+  colourRGB();
+  colourInsert();
+}
+
+function colourInsert() {
   $('body').css( "background", "linear-gradient(" + colorsCSS[0] + ", " + colorsCSS[1] + ", " + colorsCSS[2] + bluePercent + "%, " + colorsCSS[3] + ", " + colorsCSS[4] + ", " + colorsCSS[5] + ", " + colorsCSS[6] )
 }
 
+function colourRGB() {
+  for (var i = 0; i < colorsRGB.length; i++) {
+   colorsCSS[i] = 'rgb(' + colorsRGB[i][0] + ', ' + colorsRGB[i][1] + ', ' + colorsRGB[i][2] + ')'
+  }
+}
 
 function showCoords(evt){
   console.log(
